faa263fcbca79f4761dfe13e674ea50873ec8b67
SlavOK <slavok400@gmail.com>
2019-11-18 20:59:40 -0500

    updated interpreter to python 3

---
+++ infer_demographics.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+import json
+from m3inference import M3Twitter
+
+tweets = [json.loads(line) for line in open('twitter_cache/training_tweets.json', encoding="utf8").readlines()]
+
+m3twitter = M3Twitter(cache_dir="twitter_cache")
+demographics = m3twitter.infer(tweets, batch_size=30, num_workers=4)
+
+
+
