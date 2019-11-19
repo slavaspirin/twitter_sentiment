596048fcee7eb0be684f2ddd24fe33c5c55ee1a2
SlavOK <slavok400@gmail.com>
2019-11-18 21:07:43 -0500

    added a path fix

--- infer_demographics.py
+++ infer_demographics.py
@@ -8,7 +8,7 @@ tweets = [json.loads(line) for line in open('training_tweets.json', encoding="ut
 m3twitter = M3Twitter(cache_dir="twitter_cache")
 demographics = m3twitter.infer(tweets, batch_size=30, num_workers=4)
 
-
+a=1
 
 
 
