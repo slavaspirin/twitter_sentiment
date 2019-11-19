4c8ba153a1ade95d8d6610a508a7c2fecd54fa24
SlavOK <slavok400@gmail.com>
2019-11-18 22:06:31 -0500

    added a path fix

--- infer_demographics.py
+++ infer_demographics.py
@@ -1,14 +1,30 @@
 #! /usr/bin/python3
 
+import os.path
 import json
 from m3inference import M3Twitter
+from m3inference.dataset import TW_DEFAULT_PROFILE_IMG
 
-tweets = [json.loads(line) for line in open('training_tweets.json', encoding="utf8").readlines()]
+# tweets = [json.loads(line) for line in open('training_tweets.json', encoding="utf8").readlines()]
 
-m3twitter = M3Twitter(cache_dir="twitter_cache")
-demographics = m3twitter.infer(tweets, batch_size=30, num_workers=4)
+cache_path = "twitter_cache"
+tweets = []
+missing_pics = 0
+for line in open('training_tweets.json', encoding="utf8").readlines():
+    loads = json.loads(line)
+    tweets.append(loads)
 
-a=1
+    if not os.path.exists(loads['img_path']):
+        loads['img_path'] = TW_DEFAULT_PROFILE_IMG
+        missing_pics += 1
+
+print(missing_pics)
 
 
-
+#
+#
+# m3twitter = M3Twitter(cache_dir=cache_path)
+# demographics = m3twitter.infer(tweets, batch_size=30, num_workers=4)
+#
+#
+# for num, tweet in enumerate(tweets):
