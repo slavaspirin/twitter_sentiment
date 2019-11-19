7601ad62751318a24a826d363a61ec8365f0f368
SlavOK <slavok400@gmail.com>
2019-11-18 21:03:49 -0500

    added infer_demographics.py

--- infer_demographics.py
+++ infer_demographics.py
@@ -3,7 +3,7 @@
 import json
 from m3inference import M3Twitter
 
-tweets = [json.loads(line) for line in open('twitter_cache/training_tweets.json', encoding="utf8").readlines()]
+tweets = [json.loads(line) for line in open('training_tweets.json', encoding="utf8").readlines()]
 
 m3twitter = M3Twitter(cache_dir="twitter_cache")
 demographics = m3twitter.infer(tweets, batch_size=30, num_workers=4)
